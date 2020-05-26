331e875b94007c583c05c7f41318bbf7385cb22c
Garrone Joseph <joseph.garrone@ensimag.grenoble-inp.fr>
2020-05-26 19:57:47 +0200

    Initial commit

---
+++ src/test/myObject.ts
@@ -0,0 +1,17 @@
+
+
+import { assert } from "evt/tools/typeSafety";
+import * as inDepth from "evt/tools/inDepth";
+import { myObject } from "..";
+
+assert(
+    inDepth.same(
+        myObject, 
+        { "p": "FOO" }
+    )
+);
+
+console.log("PASS");
+
+
+
